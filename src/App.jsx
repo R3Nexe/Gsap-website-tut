cfa03ec1e664cfce02e36648038f5915422b48c4
R3Nexe <nishhu24@gmail.com>
2025-07-03 16:29:46 +0530

    added hero and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import gsap from "gsap";
 import { ScrollTrigger,SplitText } from "gsap/all";
 import React from "react";
+import Navbar from './components/Navbar.jsx'
+import Hero from "./components/Hero.jsx";
 
 gsap.registerPlugin(ScrollTrigger,SplitText );// to make sure both plugins can be used globally
 const App=()=>{
   return(
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-300">Hello, Web!</h1>
-    </div>
+    <main>
+      <Navbar/>
+      <Hero/>
+      <div className="h-dvh bg-black"/>
+    </main>
   )
 }
 export default App
+
