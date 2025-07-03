37dd8909ee0a09d71c70fc102133371fff385e44
R3Nexe <nishhu24@gmail.com>
2025-07-03 18:51:59 +0530

    added cocktails section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger,SplitText } from "gsap/all";
 import React from "react";
 import Navbar from './components/Navbar.jsx'
 import Hero from "./components/Hero.jsx";
+import Cocktals from "./components/Cocktails.jsx";
 
 gsap.registerPlugin(ScrollTrigger,SplitText );// to make sure both plugins can be used globally
 const App=()=>{
@@ -10,6 +11,7 @@ const App=()=>{
     <main>
       <Navbar/>
       <Hero/>
+      <Cocktals/>
     </main>
   )
 }
